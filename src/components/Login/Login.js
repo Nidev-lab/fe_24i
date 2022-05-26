c23c7a171c1d655d1b4095ac840dda27ce195010
Nicolas Gonzalez <ngonzalez@dminc.com>
2022-05-26 20:17:37 -0300

    Input agregado

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -26,6 +26,10 @@ const Login = () => {
         <label for="exampleInputPassword1" class="form-label">Password</label>
         <input type="password" class="form-control" id="exampleInputPassword1" {...register("password")}/>
       </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" {...register("password")} />
+      </div>
       <button type="submit" class="btn btn-primary">Submit</button>
     </form>
   );
